b2e12457f70ecb5255d01665483f078b53ee6275
AlexVater <alexsander.vater@gmail.com>
2021-12-13 14:47:40 -0300

    fix: adding jpa

--- src/main/java/io/github/alex/domain/repository/ClienteRepository.java
+++ src/main/java/io/github/alex/domain/repository/ClienteRepository.java
@@ -1,61 +1,11 @@
 package io.github.alex.domain.repository;
 
 import io.github.alex.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
-@Repository
-public class ClienteRepository {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
-    @Autowired
-    private EntityManager entityManager;
-
-    @Transactional
-    public Cliente save(Cliente cliente){
-        entityManager.persist(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public Cliente update(Cliente cliente){
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void delete(Cliente cliente){
-        if (!entityManager.contains(cliente)){
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void delete(Integer id){
-        Cliente cliente = entityManager.find(Cliente.class, id);
-        delete(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> findByName(String nome){
-        String jpql = " SELECT c FROM Cliente c WHERE c.nome LIKE :nome";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome + "%");
-        return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> getAll(){
-        return entityManager
-                .createQuery("FROM Cliente", Cliente.class)
-                .getResultList();
-    }
-
+    List<Cliente> findByName(String nome);
 }
